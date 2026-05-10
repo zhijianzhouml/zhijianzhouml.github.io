b6447bee44fb3d912b8b49dc3985303997982f6b
zhijianzhouml <zhijianzhou.ml@gmail.com>
2026-05-10 16:41:12 +0000

    Deploying to gh-pages from @ zhijianzhouml/zhijianzhouml.github.io@dbfce7f1efafa19f8af996e447d0e77355c49bb3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -351,16 +351,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-two-papers-have-been-accepted-to-icml-2026-see-you-in-seoul-korea",
+          title: 'Two papers have been accepted to ICML 2026. See you in Seoul, Korea!...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
